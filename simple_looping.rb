34a9f80263f432cb4a9d2904fc0917fcbfb80853
Ana Riat <t.riat24@gmail.com>
2019-05-10 03:34:26 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,7 +35,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  
+  number_of_times = 1..7
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
