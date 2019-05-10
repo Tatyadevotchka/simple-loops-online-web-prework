55220da895381ffc42aa564457669ec563aa1058
Ana Riat <t.riat24@gmail.com>
2019-05-10 03:30:47 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,7 +22,7 @@ def while_iterator(number_of_times)
  while number_of_times < 7
  puts phrase 
  number_of_times += 1 
- 
+ end
  
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
